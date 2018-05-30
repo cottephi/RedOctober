e93b7042c01dee80941057d11c1a2e40d4c634d6
cottephi <cottephi@gmail.com>
2018-05-30 17:49:47 +0200

    Changed dt to 10, added time window plot

--- PosReco_and_pvalue.cpp
+++ PosReco_and_pvalue.cpp
@@ -57,12 +57,12 @@ void PosReco_and_pvalue(){
 
   
   int Nmodules = 80;
-  vector<double> Powers = {180,150,120,90,60,30};//in MW    ,120,90,60,30
+  vector<double> Powers = {150};//in MW    ,120,90,60,30
   vector<int> GrColors = {632,600,417,401,800,433};
   double ModuleDy = 10*1e3; // in km. Distance between modules
   double zpos = 1e3; //1000 m overburden in the ocean (water)
-  double dt = 1.; //min
-  double MassTarget = 100; //in ktons
+  double dt = 10.; //min
+  double MassTarget = 50; //in ktons
   double startx = -100*1e3;
   double endx = 100*1e3;
     
@@ -104,18 +104,24 @@ void PosReco_and_pvalue(){
           modules_coordinates->SetNextPoint(0,ydet*1e-3,0);
         }
         double dist = TMath::Sqrt(xpos*xpos+ydet*ydet+zpos*zpos);
-        InstantBGEventCount = ExpBGFlux(MassTarget);
-        InstantEventCount+= dt*ExpNuFlux(dist, MassTarget, Power)*Pee2Flav(3.6,dist);
-        TotEventCount+= InstantEventCount;
-        BGEventCount+= InstantBGEventCount;
+        InstantBGEventCount += ExpBGFlux(MassTarget);
+        InstantEventCount += dt*ExpNuFlux(dist, MassTarget, Power)*Pee2Flav(3.6,dist);
+        TotEventCount += dt*ExpNuFlux(dist, MassTarget, Power)*Pee2Flav(3.6,dist);
+        BGEventCount += ExpBGFlux(MassTarget);
         if(TotEventCount+BGEventCount-1 > 1 and time_for_one_event == -1){
           time_for_one_event = time;
         }
       }
-      approachgraph.back()->SetPoint(approachgraph.back()->GetN(),time,TotEventCount);
-      approachgraphBG->SetPoint(approachgraphBG->GetN(),time,BGEventCount);
-      approachgrapSignificance.back()->SetPoint(approachgrapSignificance.back()->GetN(),time, Significance(TotEventCount,BGEventCount));
-      derivative_significance.back()->SetPoint(derivative_significance.back()->GetN(),time,Significance(InstantEventCount, InstantBGEventCount));
+      if(Powers.size()>0){
+        approachgraph.back()->SetPoint(approachgraph.back()->GetN(),time,TotEventCount);
+        approachgraphBG->SetPoint(approachgraphBG->GetN(),time,BGEventCount-1);
+      }
+      else{
+        approachgraph.back()->SetPoint(approachgraph.back()->GetN(),xpos,TotEventCount);
+        approachgraphBG->SetPoint(approachgraphBG->GetN(),xpos,BGEventCount-1);
+      }
+      approachgrapSignificance.back()->SetPoint(approachgrapSignificance.back()->GetN(),xpos, Significance(TotEventCount,BGEventCount));
+      derivative_significance.back()->SetPoint(derivative_significance.back()->GetN(),xpos,Significance(InstantEventCount, InstantBGEventCount));
       InstantEventCount = 0.;
       InstantBGEventCount = 1.;
       time += dt;
@@ -126,16 +132,19 @@ void PosReco_and_pvalue(){
     approachgraph.back()->SetMarkerColor(GrColors[i]);
     approachgraph.back()->SetLineColor(GrColors[i]);
     approachgraph.back()->SetMarkerStyle(2);
+    approachgraph.back()->SetLineWidth(5);
     approachgrapSignificance.back()->SetMarkerColor(kGreen+i);
     approachgrapSignificance.back()->SetLineColor(kGreen+i);
     approachgrapSignificance.back()->SetMarkerStyle(2);
+    approachgrapSignificance.back()->SetLineWidth(5);
     derivative_significance.back()->SetMarkerColor(kGreen+i);
     derivative_significance.back()->SetLineColor(kGreen+i);
     derivative_significance.back()->SetMarkerStyle(2);
+    derivative_significance.back()->SetLineWidth(5);
     approachgraph.back()->SetTitle(to_string_with_precision(Power,3).data());
     if(first_iteration){
-      approachgrapSignificance.back()->SetTitle("significance;t(min);#sigmas");
-      derivative_significance.back()->SetTitle(string(";t(min);#sigmas / " + to_string_with_precision(dt,1) + " minutes").data());
+      approachgrapSignificance.back()->SetTitle("significance;x(km);# #sigma");
+      derivative_significance.back()->SetTitle(string(";x(km);# #sigma / " + to_string_with_precision(dt,2) + " minutes").data());
       approachgraphBG->SetMarkerColor(kBlack);
       approachgraphBG->SetLineColor(kBlack);
       approachgraphBG->SetTitle("BG expected events");
@@ -149,21 +158,21 @@ void PosReco_and_pvalue(){
   for( int i = 0; i < approachgraph.size(); i++){
     multi_approach->Add(approachgraph[i]);
   }
-  multi_approach->SetTitle("Signal events for different reactor power;t(min);events");
+  multi_approach->SetTitle("Signal events for different reactor power;x(km);events");
   
   TCanvas* events = new TCanvas("Moving_submarine_events","moving_submarine_events",1500,750);
-  multi_approach->Draw("AP");
+  multi_approach->Draw("AC");
   approachgraphBG->Draw("same");
   leg1->Draw();
   events->SaveAs(string("modules"+to_string(Nmodules)+"_power"+to_string_with_precision(Powers[0],3)+"_km"+to_string_with_precision(endx/1e3,3)+"_mass"+to_string_with_precision(MassTarget,3)+"_events.png").data());
   
   TCanvas* significance_can = new TCanvas("Moving_submarine_pvalue","moving_submarine_pvalue",1500,750);
-  approachgrapSignificance[0]->Draw("AP");
+  approachgrapSignificance[0]->Draw("AC3");
   significance_can->SaveAs(string("modules"+to_string(Nmodules)+"_power"+to_string_with_precision(Powers[0],3)+"_km"+to_string_with_precision(endx/1e3,3)+"_mass"+to_string_with_precision(MassTarget,3)+"_pvalue.png").data());
   significance_can->SaveAs(string("modules"+to_string(Nmodules)+"_power"+to_string_with_precision(Powers[0],3)+"_km"+to_string_with_precision(endx/1e3,3)+"_mass"+to_string_with_precision(MassTarget,3)+"_pvalue.root").data());
   
   TCanvas* significance_derivative_can = new TCanvas("significance_derivative_can","significance_derivative_can",1500,750);
-  derivative_significance[0]->Draw("AP");
+  derivative_significance[0]->Draw("AC3");
   significance_derivative_can->SaveAs(string("modules"+to_string(Nmodules)+"_power"+to_string_with_precision(Powers[0],3)+"_km"+to_string_with_precision(endx/1e3,3)+"_mass"+to_string_with_precision(MassTarget,3)+"_timewindow.png").data());
   significance_derivative_can->SaveAs(string("modules"+to_string(Nmodules)+"_power"+to_string_with_precision(Powers[0],3)+"_km"+to_string_with_precision(endx/1e3,3)+"_mass"+to_string_with_precision(MassTarget,3)+"_timewindow.root").data());
   
